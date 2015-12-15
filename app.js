cdaed5eed7ba72a26cc0433595783d6760559281
Yevgeniy Teleganov <teleganov@me.com>
2015-12-15 13:48:37 -0500

    added query string to find-open-room

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ var names = {
   south: 'South'
 }
 
-function getQueryVariable(variable)
-{
+function getQueryVariable(variable) {
   var query = window.location.search.substring(1);
   var vars = query.split("&");
   for (var i=0;i<vars.length;i++) {
@@ -18,4 +17,15 @@ function getQueryVariable(variable)
     if(pair[0] == variable){return pair[1];}
   }
   return(false);
+}
+
+function updateQueryVariable(uri, key, value) {
+  var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+  var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+  if (uri.match(re)) {
+    return uri.replace(re, '$1' + key + "=" + value + '$2');
+  }
+  else {
+    return uri + separator + key + "=" + value;
+  }
 }
