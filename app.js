8890858993a1816cd0ec295206b293f387d6d991
Yevgeniy Teleganov <teleganov@me.com>
2015-12-15 13:24:01 -0500

    added error check on campus area

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-var App = angular.module('FreeSpace', [
-  'ngRoute'
-]);
+var names = {
+  blackboard: 'Blackboard',
+  whiteboard: 'Whiteboard',
+  projector: 'Projector',
+  television: 'Television',
+  tables: 'Tables',
+  desks: 'Desks',
+  north: 'North',
+  south: 'South'
+}
 
-angular.module('FreeSpace').controller('MainController', function ($scope, $http, $window) {
-  $scope.test = "hello";
-});
+function getQueryVariable(variable)
+{
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    if(pair[0] == variable){return pair[1];}
+  }
+  return(false);
+}
